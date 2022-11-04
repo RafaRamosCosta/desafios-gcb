3dc0bd2a1b31dbdc01994e9f8265acae3b1c99b7
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-04 11:27:51 -0300

    Desafio 10 concluído com tratagem de erro.

---
+++ desafio10.js
@@ -0,0 +1,33 @@
+class CommonValue {
+  constructor(array1, array2) {
+    this.array1 = array1;
+    this.array2 = array2;
+  }
+
+  getCommonValue() {
+    let commonValues = [];
+    try {
+      if (this.array1.length > 0 && this.array2.length > 0) {
+        for (let item of this.array1) {
+          if (this.array2.includes(item)) {
+            commonValues.push(item);
+          }
+        }
+        if (commonValues.length > 0) {
+          return commonValues;
+        }
+        return 'Nenhum valor em comum';
+      }
+      throw new Error('Tamanho(s) do(s) array(s) inválido(s)');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+const commonValue = new CommonValue(
+  [1, 7, 8, 4, 5],
+  [6, 7, 8, 9, 10]
+).getCommonValue();
+
+console.log(commonValue);
